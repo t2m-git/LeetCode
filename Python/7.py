968693475902dc4ec3bb814ee8cb396be9f73f05
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-22 15:30:15 -0700

    Create 7.py

---
+++ Python/7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        lst_reverse = []
+        lst_reverse = str(x)[::-1]
+
+        if lst_reverse[-1] == '-':
+            lst_reverse = lst_reverse[-1] + lst_reverse[0:-1]
+
+        x = int(lst_reverse)
+        
+        if (x <= 2**31 -1) & (x >= -2**31):
+        
+            return x
+        
+        else:
+            
+            return 0
