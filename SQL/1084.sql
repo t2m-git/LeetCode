622a4d1d41505683e3d3c2819950554aeb8f5c31
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-23 15:33:41 -0700

    Update 1084.sql

--- SQL/1084.sql
+++ SQL/1084.sql
@@ -20,3 +20,24 @@ INNER JOIN Product AS p ON s.product_id = p.product_id
 WHERE
     s.oldest_sold BETWEEN '2019-01-01' AND '2019-03-31' AND
     s.latest_sold BETWEEN '2019-01-01' AND '2019-03-31';
+
+/*
+WITH notspring as
+(
+SELECT
+    product_id
+FROM
+    Sales
+WHERE
+    sale_date NOT BETWEEN '2019-01-01' and '2019-03-31'
+)
+
+SELECT
+    DISTINCT s.product_id, p.product_name
+FROM
+    Sales s
+INNER JOIN product p ON p.product_id = s.product_id
+WHERE
+    s.sale_date BETWEEN '2019-01-01' AND '2019-03-31' AND
+    s.product_id NOT IN (select product_id from notspring);
+*/
