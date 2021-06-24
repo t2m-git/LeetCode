22f1079d90391c464e82df636cfff2c89cab86da
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-23 19:48:19 -0700

    Update 585.sql

--- SQL/585.sql
+++ SQL/585.sql
@@ -38,3 +38,34 @@ FROM
 WHERE
     i.PID IN (SELECT * FROM tmp_TIV_2015) AND
     i.PID IN (SELECT * FROM loc);
+
+/*
+SELECT
+    SUM(TIV_2016) as TIV_2016
+FROM
+    insurance
+WHERE
+    TIV_2015 IN (
+        
+        SELECT
+            TIV_2015
+        FROM
+            insurance
+        GROUP BY TIV_2015
+        HAVING
+            COUNT(*) >= 2
+        
+    )
+    AND (lat, lon) IN(
+        
+        SELECT
+            lat,
+            lon
+        FROM
+            insurance
+        GROUP BY lat, lon
+        HAVING
+            COUNT(*) = 1
+        
+    );
+ */
