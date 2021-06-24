330e618cd8d28787f2c6b5b67c06edcfe10cf8e1
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-23 17:52:19 -0700

    Update 574.sql

--- SQL/574.sql
+++ SQL/574.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 WITH cand AS (
 
 SELECT
@@ -25,3 +26,39 @@ WHERE
             cand
         
     );
+
+
+SELECT
+    c.Name
+FROM
+    Candidate AS c
+WHERE
+    id = (
+    
+        SELECT
+            v.CandidateId
+        FROM
+            Vote AS v
+        GROUP BY v.CandidateId
+        ORDER BY COUNT(v.CandidateId) DESC
+        LIMIT 1
+    
+    )
+*/
+
+SELECT
+    c.Name
+FROM
+    Candidate AS c
+INNER JOIN Vote v ON c.id = v.CandidateId
+GROUP BY v.CandidateId
+HAVING 
+    COUNT(v.CandidateId) >= (
+                            SELECT
+                                COUNT(CandidateId) AS cnt
+                            FROM
+                                Vote
+                            GROUP BY CandidateId
+                            ORDER BY cnt DESC
+                            LIMIT 1
+                            );
