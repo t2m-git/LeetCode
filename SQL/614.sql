09d5907ba893472b0aeeb37dec4b8ae954d2def2
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-24 16:41:21 -0700

    Update 614.sql

--- SQL/614.sql
+++ SQL/614.sql
@@ -1,6 +1,18 @@
 # Write your MySQL query statement below
 SELECT
     f1.followee AS follower,
+    # Why do I have to use "DISTINCT fi.follower"?
+    # This is for a case in which there are two duplicated records in data.
+    # For example, if there are two records written in the following.
+    # followee, follower
+    # ["bkwz","xlxeg"]
+    # ["bkwz","xlxeg"]
+    # ["bkwz","nhrry"]
+    # When using COUNT(f.followee), the result becomes three.
+    # When using COUNT(f.follower), the result becomes three.
+    # When using COUNT(DISTINCT f.followee), the result becomes one.
+    # When using COUNT(DISTINCT f.follower), the result becomes two.
+    # Thus, I have to use COUNT(DISTINCT f.follower).
     COUNT(DISTINCT f1.follower) AS num
 FROM
     follow AS f1
