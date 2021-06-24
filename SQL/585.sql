1b30982d9f32a37ad53bf5cbf12e84f6f9ba41b6
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-23 19:44:16 -0700

    Create 585.sql

---
+++ SQL/585.sql
@@ -0,0 +1,40 @@
+# Write your MySQL query statement below
+WITH tmp_TIV_2015 AS (
+
+SELECT
+    PID
+FROM
+    insurance
+WHERE
+    TIV_2015 IN (
+    
+    SELECT
+        TIV_2015
+    FROM 
+        insurance
+    GROUP BY TIV_2015
+    HAVING
+        COUNT(TIV_2015) > 1    
+    )
+
+),
+
+loc AS (
+
+SELECT
+    PID
+FROM
+    insurance
+GROUP BY LAT, LON
+HAVING
+    COUNT(LAT & LON) = 1
+
+)
+
+SELECT
+    SUM(i.TIV_2016) AS `TIV_2016`
+FROM
+    insurance AS i
+WHERE
+    i.PID IN (SELECT * FROM tmp_TIV_2015) AND
+    i.PID IN (SELECT * FROM loc);
