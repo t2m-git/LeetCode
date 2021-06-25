dffb50b21cab128703b069635f089a3b1255a3af
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-25 11:56:13 -0700

    Update 1113.sql

--- SQL/1113.sql
+++ SQL/1113.sql
@@ -1,3 +1,15 @@
+SELECT
+    extra AS report_reason,
+    # Why do I have to use DISTINCT for post_id?
+    COUNT(DISTINCT post_id) AS report_count
+FROM
+    Actions
+WHERE
+    action_date = '2019-07-04' AND
+    action = 'report'
+GROUP BY extra;
+
+/*
 SELECT 
     x.extra AS report_reason,
     COUNT(x.extra) AS report_count 
@@ -18,3 +30,4 @@ FROM
         x.action = 'report'
     GROUP BY x.extra
     ORDER BY report_reason ASC;
+*/
