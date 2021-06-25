13a83919d6e950c7c00a6cf5fc0c3b9195066c63
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-24 17:47:57 -0700

    Update 1045.sql

--- SQL/1045.sql
+++ SQL/1045.sql
@@ -6,4 +6,5 @@ FROM
 LEFT JOIN
     Product AS p ON p.product_key = c.product_key
 GROUP BY c.customer_id
-HAVING COUNT(DISTINCT p.product_key) = (SELECT COUNT(product_key) FROM product);
+# DISTINCT clause for product table is just for insurance.
+HAVING COUNT(DISTINCT p.product_key) = (SELECT COUNT(DISTINCT product_key) FROM product);
