bf04488dad4787420ad4e0f8e22bdd881c4084e4
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-25 13:01:02 -0700

    Update 1098.sql

--- SQL/1098.sql
+++ SQL/1098.sql
@@ -5,9 +5,14 @@ SELECT
 FROM
     Books AS b
 LEFT JOIN Orders AS o ON o.book_id = b.book_id
+# The following code is to keep records with Null value in Books table.
 AND o.dispatch_date BETWEEN '2019-06-23' - INTERVAL 1 YEAR AND '2019-06-23'
+#AND DATEDIFF('2019-06-23', O.dispatch_date) < 365  
 WHERE
     b.available_from < ('2019-06-23' - INTERVAL 30 DAY)
+    #DATEDIFF('2019-06-23', B.available_from) > 30
 GROUP BY b.book_id
 HAVING
+    
     SUM(coalesce(o.quantity,0)) < 10;
+    #IFNULL(SUM(o.quantity),0) < 10;
