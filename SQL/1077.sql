c99064a8fafc05d42fab8b6cde4f78adcd95573e
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-25 11:25:15 -0700

    Update 1077.sql

--- SQL/1077.sql
+++ SQL/1077.sql
@@ -19,3 +19,23 @@ FROM
 INNER JOIN Employee AS e ON e.employee_id = c.employee_id
 WHERE
     e.experience_years = c.max_exp;
+
+/*
+SELECT
+    p.project_id,
+    p.employee_id
+FROM 
+    Project AS p
+LEFT JOIN Employee AS e on p.employee_id = e.employee_id,
+(
+    SELECT
+        p.project_id,
+        MAX(e.experience_years) as num
+    FROM Project AS p
+    LEFT JOIN Employee e on p.employee_id = e.employee_id
+    GROUP BY p.project_id
+) as temp
+WHERE
+    p.project_id = temp.project_id
+    AND e.experience_years = temp.num;
+*/
