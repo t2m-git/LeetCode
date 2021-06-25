b80d2bf3a6e64d26735b54e61be53ececb854793
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-24 17:25:32 -0700

    Update 626.sql

--- SQL/626.sql
+++ SQL/626.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT
+/*SELECT
     (CASE
         WHEN MOD(id, 2) <> 0 AND seat_counts.counts != id THEN id + 1
         WHEN MOD(id, 2) <> 0 AND seat_counts.counts = id THEN id
@@ -18,3 +18,17 @@ FROM
     ) AS seat_counts
     
 ORDER BY id ASC;
+*/
+
+SELECT
+    (CASE
+        # When id is even number, just replace id with id -1
+        WHEN id % 2 = 0 THEN id - 1
+        # When id is odd number, replace id with id + 1 except for the last record
+        WHEN MOD(id, 2) = 1 AND id < (SELECT COUNT(*) from seat) then id+1
+        ELSE id
+    END) AS id,
+    student
+FROM
+    seat
+ORDER BY id;
