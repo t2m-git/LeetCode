6c29cc7b56cdcb1d488558280954ce1ea584644b
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-24 17:17:37 -0700

    Create 626.sql

---
+++ SQL/626.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT
+    (CASE
+        WHEN MOD(id, 2) <> 0 AND seat_counts.counts != id THEN id + 1
+        WHEN MOD(id, 2) <> 0 AND seat_counts.counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (
+    
+    SELECT
+        COUNT(*) AS counts
+    FROM
+        seat
+    
+    ) AS seat_counts
+    
+ORDER BY id ASC;
