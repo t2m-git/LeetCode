a5e12c7e0b95cb38141e85519a62406be109ea0f
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-26 20:55:58 -0700

    Update 1142.sql

--- SQL/1142.sql
+++ SQL/1142.sql
@@ -28,3 +28,33 @@ FROM
     GROUP BY user_id
     
     ) AS a2;
+
+/*
+WITH cte AS(
+    
+SELECT
+    COUNT(DISTINCT user_id) AS user_cnt, 
+    COUNT(DISTINCT session_id) AS sess_cnt
+FROM activity
+WHERE  DATEDIFF('2019-07-27', activity_date) < 30
+    
+)
+
+
+SELECT
+    ROUND(coalesce((sess_cnt/user_cnt),0),2) as average_sessions_per_user 
+FROM cte;
+
+SELECT
+    IFNULL(
+        (  
+            select
+                ROUND(((COUNT(DISTINCT session_id))/(COUNT(DISTINCT user_id))),2)
+            FROM
+                activity
+            WHERE
+                activity_date BETWEEN "2019-06-28" AND "2019-07-27"
+        )
+    , 0)
+AS average_sessions_per_user;
+*/
