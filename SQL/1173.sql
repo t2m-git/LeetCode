85e029fb1fabfddebbea70a365af7025ab8b4f16
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-27 00:24:22 -0700

    Update 1173.sql

--- SQL/1173.sql
+++ SQL/1173.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+/*
 SELECT
     ROUND(d2.cnt / COUNT(d1.order_date) * 100, 2) AS immediate_percentage
 FROM
@@ -14,3 +14,17 @@ FROM
         order_date = customer_pref_delivery_date
         
     ) AS d2;
+*/  
+ 
+SELECT   
+    ROUND(SUM(test) / COUNT(test)*100, 2) AS immediate_percentage
+FROM
+    (
+    SELECT 
+        CASE 
+            WHEN order_date = customer_pref_delivery_date THEN 1 
+            ELSE 0 
+        END AS test 
+    from 
+        Delivery
+    ) AS temp;
