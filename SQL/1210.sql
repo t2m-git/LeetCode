7dac00d863b46e9df31fdfcfb2d5ac29642f291e
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-28 07:29:43 -0700

    Update 1210.sql

--- SQL/1210.sql
+++ SQL/1210.sql
@@ -17,12 +17,21 @@ FROM
     
     ) AS q
 ORDER BY q.query_name ASC;
-*/
 
 SELECT
     DISTINCT query_name,
     ROUND(SUM(rating/position) / COUNT(*), 2) AS quality,
     ROUND(SUM(IF(rating<3, 1, 0))*100/COUNT(rating), 2) AS poor_query_percentage
+FROM
+    Queries
+GROUP BY query_name;
+*/
+
+
+SELECT
+    query_name,
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100, 2) AS poor_query_percentage
 FROM
     Queries
 GROUP BY query_name;
