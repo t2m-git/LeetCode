59f56a6a0461b4ad8b0ae19fac66dc03e8c4d0ca
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-28 07:21:21 -0700

    Create 1210.sql

---
+++ SQL/1210.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT
+    DISTINCT q.query_name,
+    q.quality AS quality,
+    q.poor_query_percentage AS poor_query_percentage
+FROM
+    (
+    
+    SELECT
+        query_name,
+        ROUND(SUM(rating/position) / COUNT(*), 2) AS quality,
+        ROUND(SUM(IF(rating<3, 1, 0))*100/COUNT(rating), 2) AS poor_query_percentage
+    FROM
+        Queries
+    GROUP BY query_name
+    
+    ) AS q
+ORDER BY q.query_name ASC;
