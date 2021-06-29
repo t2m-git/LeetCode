76540e52dcdbe2c41df19ea53611964d800b0085
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-29 14:09:21 -0700

    Update 1112.sql

--- SQL/1112.sql
+++ SQL/1112.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT
     p.student_id,
     p.course_id,
@@ -17,3 +18,25 @@ FROM
     ) AS p
 WHERE
     p.my_rank = 1;
+*/
+
+#Bad example, but this code works...
+#This is because group by in the last summarizes data, fetching a first record implicitely.
+WITH x AS
+    (SELECT
+        student_id,
+        course_id,
+        grade, 
+        MAX(grade) over(PARTITION BY student_id ) as mgrade
+    FROM
+        Enrollments 
+    ORDER BY student_id ASC, course_id ASC)
+
+SELECT
+    student_id,
+    course_id,
+    grade 
+FROM
+    x
+WHERE grade = mgrade
+GROUP BY student_id;
