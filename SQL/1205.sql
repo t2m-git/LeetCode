07b61a6b6a5b6b686136065083d07b35da355240
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-06-29 22:52:18 -0700

    Update 1205.sql

--- SQL/1205.sql
+++ SQL/1205.sql
@@ -49,3 +49,58 @@ HAVING
     OR SUM(u.chargeback_count) <> 0
     OR SUM(u.chargeback_amount) <> 0
 ORDER BY u.month, u.country;
+
+/*
+SELECT
+    u.month,
+    u.country,
+    SUM(u.approved_count) AS approved_count,
+    SUM(u.approved_amount) AS approved_amount,
+    SUM(u.chargeback_count) AS chargeback_count,
+    SUM(u.chargeback_amount) AS chargeback_amount
+FROM
+(   
+(
+SELECT
+    SUBSTR(t.trans_date, 1, 7) AS month,
+    t.country,
+    COUNT(*) AS approved_count,
+    SUM(t.amount) AS approved_amount,
+    0 AS chargeback_count,
+    0 AS chargeback_amount
+FROM
+    Transactions AS t
+WHERE
+    t.state = 'approved'
+GROUP BY month, t.country
+)
+UNION ALL
+(
+SELECT
+    SUBSTR(c.trans_date, 1, 7) AS month,
+    t.country,
+    0 AS approved_count,
+    0 AS approved_amount,
+    COUNT(*) AS chargeback_count,
+    SUM(t.amount) AS chargeback_amount
+FROM
+    Transactions AS t
+LEFT JOIN Chargebacks AS c ON c.trans_id = t.id
+WHERE
+    t.id IN (
+        SELECT
+            trans_id
+        FROM
+            Chargebacks
+        )
+GROUP BY month, t.country
+)
+) AS u
+GROUP BY u.month, u.country
+HAVING
+    SUM(u.approved_count) <> 0
+    OR SUM(u.approved_amount) <> 0
+    OR SUM(u.chargeback_count) <> 0
+    OR SUM(u.chargeback_amount) <> 0
+ORDER BY u.month, u.country;
+*/
