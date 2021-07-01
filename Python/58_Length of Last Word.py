8929d1298d45e052be60d10259d1504432f2781c
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-01 10:22:11 -0700

    Update 58_Length of Last Word.py

--- Python/58_Length of Last Word.py
+++ Python/58_Length of Last Word.py
@@ -4,12 +4,36 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        
+        """
+        #Solution 1 
         s = s.strip()
-        lst_word = s.split(' ')
+        lst_words = s.split(' ')
                 
         if s == " ":
             return 0
         
         else:
-            return len(lst_word[-1])
+            return len(lst_words[-1])
+        """
+        
+        """
+        #Solution 2
+        try:
+            return len(s.split()[-1])
+        
+        except IndexError:
+            return 0
+        """
+        
+        #Solution 3
+        
+        cnt = 0
+        for i in range(len(s) - 1, -1, -1):
+          
+            if s[i] != " ":
+                while i >= 0 and s[i] != " ":
+                    cnt += 1
+                    i -= 1
+                break
+        
+        return cnt
