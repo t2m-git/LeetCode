f94d227552be99a9dd0b460ab6619ee68a3b0ea2
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-01 08:03:18 -0700

    Update 1565.sql

--- SQL/1565.sql
+++ SQL/1565.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
 SELECT
-    LEFT(order_date, 7) AS month,
-    COUNT(invoice) AS order_count,
+    #LEFT(order_date, 7) AS month,
+    #SUBSTR(order_date, 1, 7) AS month,
+    DATE_FORMAT(order_date, '%Y-%m') AS month,
+    COUNT(order_id) AS order_count,
     COUNT(DISTINCT customer_id) AS customer_count
 FROM
     Orders
