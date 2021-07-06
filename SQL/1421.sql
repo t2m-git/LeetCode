c88fe96c06fbfa54d2925b042b1765aaf7419cfc
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-05 17:16:48 -0700

    Create 1421.sql

---
+++ SQL/1421.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    q.id,
+    q.year,
+    IFNULL(n.npv, 0) AS npv
+FROM
+    Queries AS q
+LEFT JOIN NPV AS n ON n.id = q.id
+AND n.year = q.year;
