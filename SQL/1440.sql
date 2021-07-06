13dc86cd5e97faecb198789b06880ee68ad91d01
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-05 17:59:21 -0700

    Update 1440.sql

--- SQL/1440.sql
+++ SQL/1440.sql
@@ -4,11 +4,18 @@ SELECT
     e.operator,
     e.right_operand,
     #v2.value,
+    /*
     CASE
-        WHEN e.operator = '>' THEN IF(v1.value > v2.value, 'true', 'false')
-        WHEN e.operator = '=' THEN IF(v1.value = v2.value, 'true', 'false')
-        WHEN e.operator = '<' THEN IF(v1.value < v2.value, 'true', 'false')
-        ELSE null END AS value
+        #WHEN e.operator = '>' THEN IF(v1.value > v2.value, 'true', 'false')
+        #WHEN e.operator = '=' THEN IF(v1.value = v2.value, 'true', 'false')
+        #WHEN e.operator = '<' THEN IF(v1.value < v2.value, 'true', 'false')
+        #ELSE null END AS value
+    */
+    CASE
+        WHEN (e.operator = '>' AND v1.value > v2.value) THEN 'true' 
+        WHEN (e.operator = '=' AND v1.value = v2.value) THEN 'true' 
+        WHEN (e.operator = '<' AND v1.value < v2.value) THEN 'true' 
+        ELSE 'false' END AS value
 FROM
     Expressions AS e
 INNER JOIN Variables AS v1 ON v1.name = e.left_operand
