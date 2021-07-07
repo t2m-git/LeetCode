843b511a7c56e1ee0e5b99ef65e42864878f67a0
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-07 06:28:29 -0700

    Update 1661.sql

--- SQL/1661.sql
+++ SQL/1661.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT
     s1.machine_id,
     ROUND(SUM(s2.timestamp - s1.timestamp) / COUNT(DISTINCT s1.machine_id, s1.process_id), 3) AS processing_time
@@ -26,3 +27,16 @@ WHERE
 ) AS s2 ON s2.machine_id = s1.machine_id
 AND s2.process_id = s1.process_id
 GROUP BY s1.machine_id;
+*/
+
+SELECT
+    DISTINCT a1.machine_id,
+    ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM
+    Activity AS a1
+INNER JOIN Activity AS a2 ON a2.machine_id = a1.machine_id 
+AND a2.process_id = a1.process_id 
+AND a1.activity_type = 'start' 
+AND a2.activity_type = 'end'
+GROUP BY a1.machine_id
+ORDER BY a1.machine_id;
