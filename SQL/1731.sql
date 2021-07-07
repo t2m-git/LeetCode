920f746d5e415605ad13cc39df53c35d537ac063
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-07 16:06:21 -0700

    Update 1731.sql

--- SQL/1731.sql
+++ SQL/1731.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT
     s.employee_id AS employee_id,
     s.name AS name,
@@ -19,3 +20,42 @@ FROM
     ) AS s
 GROUP BY 1
 ORDER BY 1;
+
+SELECT
+    e1.employee_id,
+    e1.name, 
+    COUNT(e2.employee_id) as reports_count,
+    ROUND(AVG(e2.age)) as average_age
+FROM
+    Employees AS e1
+#INNER JOIN Employees e2 ON e2.reports_to = e1.employee_id
+LEFT JOIN Employees AS e2 ON e2.reports_to = e1.employee_id
+GROUP BY e1.employee_id
+HAVING COUNT(e2.reports_to) >= 1
+ORDER BY e1.employee_id;
+
+*/
+
+
+WITH cte as (
+
+SELECT
+    reports_to,
+    COUNT(employee_id) AS reports_count,
+    round(AVG(age)) AS average_age
+FROM 
+    Employees
+WHERE reports_to IS NOT NULL
+GROUP BY reports_to
+
+)
+
+SELECT
+    employee_id,
+    name,
+    reports_count,
+    average_age
+FROM
+    cte
+INNER JOIN Employees AS e on  e.employee_id = cte.reports_to
+ORDER BY e.employee_id';
