69073786d732acfac4d1d3dd8137a26205398987
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-07 11:52:57 -0700

    Update 1699.sql

--- SQL/1699.sql
+++ SQL/1699.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT
     s.person1,
     s.person2,
@@ -7,8 +8,11 @@ SELECT
 FROM
 (
 SELECT
-    DISTINCT from_id AS person1, to_id AS person2,
-    COUNT(from_id & to_id) AS call_count,
+    #DISTINCT from_id AS person1, to_id AS person2,
+    from_id AS person1,
+    to_id AS person2,
+    #COUNT(from_id & to_id) AS call_count,
+    COUNT(*) AS call_count,
     SUM(duration) AS total_duration
 FROM
     Calls
@@ -16,8 +20,11 @@ WHERE from_id <= to_id
 GROUP BY from_id, to_id
 UNION ALL
 SELECT
-    DISTINCT to_id AS person1, from_id AS person2,
-    COUNT(to_id & from_id) AS call_count,
+    #DISTINCT to_id AS person1, from_id AS person2,
+    to_id AS person1,
+    from_id AS person2,
+    #COUNT(to_id & from_id) AS call_count,
+    COUNT(*) AS call_count,
     SUM(duration) AS total_duration
 FROM
     Calls
@@ -25,3 +32,13 @@ WHERE from_id > to_id
 GROUP BY to_id, from_id
 ) AS s
 GROUP BY 1, 2;
+*/
+
+SELECT
+    from_id AS person1,
+    to_id AS person2,
+    COUNT(*) AS call_count,
+    SUM(duration) AS total_duration
+FROM
+    calls
+GROUP BY CASE WHEN from_id < to_id THEN Concat(from_id, to_id) ELSE Concat(to_id, from_id) END;
