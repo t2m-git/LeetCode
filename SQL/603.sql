8320af19ce1b86a2b6bea7fd92c047e26f8689af
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-07 17:30:30 -0700

    Update 603.sql

--- SQL/603.sql
+++ SQL/603.sql
@@ -17,3 +17,22 @@ FROM
 WHERE
     prev_free_seat+1 = seat_id
     OR next_free_seat-1 = seat_id;
+ 
+ /*
+ # Write your MySQL query statement below
+SELECT
+    s.seat_id
+FROM
+(
+SELECT
+    *,
+    LAG(free, 1) OVER (ORDER BY seat_id) AS lag1,
+    LEAD(free, 1) OVER (ORDER BY seat_id) AS lead1
+FROM
+    cinema
+) AS s
+WHERE 
+    (s.free = 1 AND s.lag1 = 1)
+    OR (s.lead1 = 1 AND s.free = 1)
+ORDER BY 1;
+*/
