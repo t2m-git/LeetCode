c1f6f45c7dbeabd6d24f5e1f4b87afff053bd112
t2m-git <86038324+t2m-git@users.noreply.github.com>
2021-07-17 16:45:50 -0700

    Create 70_Climbing Stairs.py

---
+++ Python/70_Climbing Stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return [1, 2][n - 1]
+        first = 1
+        second = 2
+        cnt = 2
+        while cnt < n:
+            cnt += 1
+            cur = first + second
+            if cnt == n:
+                return cur
+            first = second
+            second = cur
